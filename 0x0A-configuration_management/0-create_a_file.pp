02bbc10e9743ebf38f7483f34ba413684c0950b6
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-06-29 01:15:28 +0800

    first commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Define a file resource to create /tmp/school with specific attribute
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet\n",
+}
+
