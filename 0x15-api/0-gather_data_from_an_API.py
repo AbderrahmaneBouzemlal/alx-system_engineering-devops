c9666b63eb6385578251ce292666f891bb185c56
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-07-30 00:45:31 +0800

    added some changes to the file"

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,12 +9,14 @@ import requests
 import sys
 
 
-EMPLOYEE_ID = sys.argv[1]
 url = "https://jsonplaceholder.typicode.com/users/"
 
 
 if __name__ == '__main__':
     """returns information about his/her TODO list progress"""
+    if len(sys.argv) < 2:
+        sys.exit()
+    EMPLOYEE_ID = sys.argv[1]
     res = requests.get(f"{url}{EMPLOYEE_ID}")
     response = requests.get(f"{url}{EMPLOYEE_ID}/todos")
     name = res.json()['name']
